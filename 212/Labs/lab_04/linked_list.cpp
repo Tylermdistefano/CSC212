ce728c0e80cc08977239dbc74585ac0a2f7aaa85
Tyler Distefano <tylermdistefano@uri.edu>
2023-09-28 11:13:41 -0400

    more changes

--- 212/Labs/lab_04/linked_list.cpp
+++ 212/Labs/lab_04/linked_list.cpp
@@ -4,20 +4,21 @@
 
 LinkedList::LinkedList(){
     this ->head = new Node();
-    this ->size = 0;
+    this ->size = 1;
 }
 //test for git hub
 
 LinkedList::LinkedList(int data){
- this-> head = new Node(data);
+ this-> head = new Node(data); 
  this-> size = 1;
 
 }
 
 
 LinkedList::LinkedList(std::vector<int> vec){
-    this -> size = vec.size();
-    for(unsigned int i = 0 ; i < vec.size() ; i++){
+    this->head = new Node(vec[0]);
+    this->size = 1;
+    for(unsigned int i = 1 ; i < vec.size() ; i++){
         push_back(vec[i]);
     }
     
@@ -25,29 +26,84 @@ LinkedList::LinkedList(std::vector<int> vec){
 }
 
 LinkedList::~LinkedList(){
-
+while (head != nullptr) {
+        Node* temp = head;
+        head = head->next;
+        delete temp;
+    }
 }
 
 
 void LinkedList::push_front(int data){
-
+    Node* newNode = new Node(data);
+    newNode->next = head;
+    head = newNode;
+    size++;
 }
 
 void LinkedList::push_back(int data){
+ Node* newNode = new Node(data);
 
+    if (head == nullptr) {
+        head = newNode;
+    } else {
+        Node* current = head;
+        while (current->next != nullptr) {
+            current = current->next;
+        }
+        current->next = newNode;
+    }
+    size++;
+       
 }
 
 void LinkedList::insert(int data, int idx){
+Node* current = head;
+for(int i = 0 ; i < idx-1 ; i++){
+    current = current->next;
+}
+ Node* newNode = new Node(data);
+ newNode->next = current->next;
+        current->next = newNode;
+        size++;
+
 
 }
 
 void LinkedList::remove(int data){
+ Node* current = head;
+    if(current->data == data){
+        Node* temp = head;
+        head = head->next;
+        delete temp;
+        size--;
+        return;
+        }
 
+ Node* current = head;
+    while (current->next != nullptr && current->next->data != data) {
+        current = current->next;
+    }
+    
+    if (current->next != nullptr) {
+        Node* temp = current->next;
+        current->next = current->next->next;
+        delete temp;
+        size--;
+    }
+
+    
 }
 
 
 bool LinkedList::contains(int data){
-
+    Node* current = head;
+        while (current->next != nullptr) {
+            if(current->data == data){
+                return true;
+            }
+        }
+    return false;   
 }
 
 
--- 212/Labs/lab_04/main.cpp
+++ 212/Labs/lab_04/main.cpp
@@ -4,6 +4,41 @@
 #include <sstream>
 #include <iostream>
 
-int main(int argc, char* argv[]){
+int main(int argc, char* argv[]) {
 
+    std::string file_name = argv[1];
+    std::cout << file_name << std::endl;
+    std::ifstream inputFile(file_name);
+    std::vector<std::string> rows;
+    std::string line;
+    
+    while (getline(inputFile, line)) {
+        rows.push_back(line);
+    }
+
+    inputFile.close();
+
+    LinkedList *list;
+    if (rows[0].length() < 1){
+    list = new LinkedList();
+    
+    }
+    else if (rows[0].length()== 1){
+        LinkedList(stoi(rows[0]));
+    }
+    else{
+        std::vector<int> first_line;
+        for (int i =0 ; i < rows[0].length() ; i++){
+            stoi(rows[0][i]);
+            first_line.push_back();
+        }
+
+
+    }
+    return 0;
 }
+
+
+
+
+
